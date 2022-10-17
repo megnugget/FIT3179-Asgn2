f689d64c5505ec1dfd542ea9b5bccfc2f168ae0d
megnugget <114331457+megnugget@users.noreply.github.com>
2022-10-17 16:28:44 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,19 @@
+var vis1= "choropleth_map.json";
+vegaEmbed('#choropleth_map',vis1,{"actions":false}).then(function(result){
+
+}).catch(console.error);
+
+var vis2= "sankey_diagram.json";
+vegaEmbed('#sankey_diagram',vis2,{"actions":false}).then(function(result){
+
+}).catch(console.error);
+
+var vis3= "line_chart.json";
+vegaEmbed('#line_chart',vis3,{"actions":false}).then(function(result){
+
+}).catch(console.error);
+
+var vis4= "donut_chart.json";
+vegaEmbed('#donut_chart',vis4,{"actions":false}).then(function(result){
+
+}).catch(console.error);
